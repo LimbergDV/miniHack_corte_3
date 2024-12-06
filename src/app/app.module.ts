cd31b365d11a57363ec685f7273ef97bca63238e
JulioAlb453 <fuliobeto18@gmail.com>
2024-12-06 13:48:58 -0600

    add character.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';  // Importa HttpClientModule
+import { HttpClientModule } from '@angular/common/http';  
 
 import { HomeModule } from './home/home.module';
 import { AppComponent } from './app.component';
