c83503d04e4baddd99e69528962367d68b9436f9
LimbergDV <limbergdelvalle@gmail.com>
2024-12-06 13:15:31 -0600

    add: adding cards and modals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CardsModule } from './cards/cards.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    CardsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cards/cards-characters/cards-characters.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { CharacterService } from '../../service/character.service';
+
+@Component({
+  selector: 'app-cards-characters',
+  templateUrl: './cards-characters.component.html',
+  styleUrl: './cards-characters.component.css'
+})
+export class CardsCharactersComponent implements OnInit {
+  characters: any[] = [];
+  selectedCharacter: any = null;
+
+  constructor(private caractersService: CharacterService) {}
+
+  ngOnInit(): void {
+    this.caractersService.getAllCharacters().subscribe(
+      (response) => {
+        this.characters = response.results;
+      },
+      (error) => {
+        console.error('Error fetching characters:', error);
+      }
+    );
+  }
+
+  openModal(character: any){
+    this.selectedCharacter = character;
+  }
+
+  closeModal(): void{
+    this.selectedCharacter = null;
+  }
+
+}
